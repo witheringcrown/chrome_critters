34551b54658e8d805cb52b500f3dfc7d1616f903
Dakota Vaughan <witheringcrown@gmail.com>
2025-09-27 22:13:30 -0400

    Cracked Assets

--- src/lilguy.tsx
+++ src/lilguy.tsx
@@ -3,13 +3,14 @@
 import mon from './assets/creature.gif';
 import dead from './assets/dead.gif';
 import egg from './assets/egg.gif';
-    
+import scrambled from './assets/cracked.png';
+
 type LilGuyProps = {
-    imageState : 'mon' | 'dead' | 'egg';
+    imageState : 'mon' | 'dead' | 'egg' | 'scrambled';
 };
 
 function LilGuy({ imageState }: LilGuyProps) {
-    const currentImage = imageState === 'dead' ? dead : imageState === 'mon' ? mon : egg;
+    const currentImage = imageState === 'dead' ? dead : imageState === 'mon' ? mon : imageState === 'scrambled' ? scrambled : egg;
 
     return (
         <div>
